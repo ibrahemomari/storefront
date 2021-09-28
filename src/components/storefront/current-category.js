249aeb2aaa579b74d36c62aa8121190b0a6cfe23
ibrahemomari <ibrahem.omari96@gmail.com>
2021-09-28 16:50:10 +0300

    lab 38

--- src/components/storefront/current-category.js
+++ src/components/storefront/current-category.js
@@ -5,9 +5,9 @@ const ActiveCategory = (props) => {
   return <h1>{props.ActiveCategory}</h1>;
 };
 
-// const mapStateToProps = (state) => ({
-//   ActiveCategory: state.categories.activeCategory,
-// });
+const mapStateToProps = (state) => ({
+  ActiveCategory: state.categories.activeCategory,
+});
 
-const mapStateToProps = (state) => (console.log("shjfhdjhfs",state))
+
 export default connect(mapStateToProps)(ActiveCategory);
